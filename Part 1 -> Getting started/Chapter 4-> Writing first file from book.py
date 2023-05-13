707eb586cb678e528074a7e3801784668e582188
anishgorakavi787809 <34226544+anishgorakavi787809@users.noreply.github.com>
2023-05-13 00:12:12 -0500

    Finished reading chapter 4

--- Part 1 -> Getting started/Chapter 4-> Writing first file from book.py
+++ Part 1 -> Getting started/Chapter 4-> Writing first file from book.py
@@ -1,3 +1,4 @@
+#4.2.1
 var = (10 +
        2 + 
        10 
@@ -6,7 +7,25 @@ var = (10 +
 print(var)
 
 x = 123
+#4.2
 if x > 10:
     print("X slay")
     print("Sec line")
-    
+
+v = "Hello / World"
+print(v)
+
+#4.2.2
+print("Hello");print("World")
+if x > 0: print("not -")
+
+#4.4
+
+#A small comment block
+
+"""
+A
+Giant
+Comment
+Block!!!!!    
+"""
