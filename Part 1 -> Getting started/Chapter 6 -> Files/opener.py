f070a63e8f4c1b001b5cdd55ae7aeac8602209bd
anishgorakavi787809 <34226544+anishgorakavi787809@users.noreply.github.com>
2023-05-14 19:01:38 -0500

    e

--- Part 1 -> Getting started/Chapter 6 -> Files/opener.py
+++ Part 1 -> Getting started/Chapter 6 -> Files/opener.py
@@ -6,7 +6,16 @@ for d in f.readlines():
     print(d.strip("\n"))
 f.close()
 
+#6.2.2 -> Read file
 # Best way is with "with".
-
+dic = {}
 with open("/Users/anishgorakavi/Desktop/Python 3 Book adventure/Part 1 -> Getting started/Chapter 6 -> Files/names.txt") as fi:
-    pass
+    for f in fi.readlines():
+        f = f.split(" ")
+        dic[f[0]] = f[1].strip("\n")
+
+print(dic)
+#6.2.3 -> Read file
+with open("/Users/anishgorakavi/Desktop/Python 3 Book adventure/Part 1 -> Getting started/Chapter 6 -> Files/wrike.txt","w") as wri:
+    
+    wri.writelines([f"{di} {dic[di]}\n" for di in dic])
