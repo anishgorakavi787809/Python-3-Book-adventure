8af5c027615fab7f4ac24f80c07a065a6c50255e
anishgorakavi787809 <34226544+anishgorakavi787809@users.noreply.github.com>
2023-08-15 22:15:48 -0500

    d

--- Part 1 -> Getting started/Chapter 6 -> Files/bmper.py
+++ Part 1 -> Getting started/Chapter 6 -> Files/bmper.py
@@ -1,19 +1,6 @@
+d = 2
 
-from statistics import *
+def d():
+    print(3)
 
-
-arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-
-def bin_ser(arr,goal):
-    low,high = 0,len(arr) - 1
-    while True:
-       
-        med = (low + high) // 2
-        print(med)
-        if arr[med] == goal: return med
-        elif arr[med] < goal: low = med + 1
-        else: high = med - 1
-        if low > high: break
-    return -1
-
-print(bin_ser(arr,40))
+print(d)
